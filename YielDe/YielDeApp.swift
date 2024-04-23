255d77af84ef61a147d6672e5d6aa74badd7c817
Kilo Loco <kyle@kiloloco.com>
2024-04-22 22:47:19 -0700

    Initial Commit

---
+++ YielDe/YielDeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YielDeApp.swift
+//  YielDe
+//
+//  Created by Kilo Loco on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct YielDeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
